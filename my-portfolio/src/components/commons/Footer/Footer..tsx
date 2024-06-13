8140e1d38d7b4ef08c9484c71269d043868b8967
Rauã <rauavasconcelos22@gmail.com>
2024-06-13 14:41:53 -0300

    inserindo links no footer e Head no minhaHistoria

--- my-portfolio/src/components/commons/Footer/Footer..tsx
+++ my-portfolio/src/components/commons/Footer/Footer..tsx
@@ -6,14 +6,14 @@ function Footer() {
         <footer className="border-t-[3px] border-opacity-70 px-4 py-3 flex justify-between">
             <strong className="text-xl">Rauã Hipólito</strong>
             <div className="flex items-center gap-4" >
-                <a href="" target="_blank" >
+                <a href="https://github.com/raua-hipolito" target="_blank" >
                     <Image src="/github.png" alt="GitHub" width={26} height={26}
                     />
                 </a>
-                <a href="" target="_blank">
+                <a href="https://www.linkedin.com/in/devrauahipolito/" target="_blank">
                     <Image src="/linkedin.png" alt="LinkedIn" width={26} height={26} />
                 </a>
-                <a href="">
+                <a href="https://www.instagram.com/__rhipolito/" target="__blank">
                     <Image src="/instagram.png" alt="Instagram" width={26} height={26} />
                 </a>
             </div>
--- my-portfolio/src/pages/minhaHistoria.tsx
+++ my-portfolio/src/pages/minhaHistoria.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 
 function minhaHistoria() {
     return (
-<div className="container mx-auto p-4 ">
+    <div className="container mx-auto p-4 ">
+        <Head>
+            <title>Minha história | Hipólito</title>
+        </Head>
       <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[450px]">
         <Image
           src="/certificado.png" // Substitua pelo caminho da sua imagem
@@ -34,23 +37,9 @@ function minhaHistoria() {
       <h2 className="text-2xl font-bold mb-4 px-5 py-4">
         Sou formado em Desenvolvimento Full-Stack ,um curso onde pude aprender tecnológias esseciais pro mercado de trabalho.
       </h2>
-      <Footer />
+        <Footer /> 
     </div>
     )
 }
 
 export default minhaHistoria;
-
-
-<div className="container mx-auto p-4">
-<h1 className="text-2xl font-bold mb-4">Imagem Responsiva com Next.js e Tailwind CSS</h1>
-<div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[500px]">
-  <Image
-    src="/path/to/your/image.jpg" // Substitua pelo caminho da sua imagem
-    alt="Descrição da imagem"
-    layout="fill"
-    objectFit="contain" // Ajuste para 'contain' se quiser que a imagem seja totalmente visível
-    className="rounded-lg"
-  />
-</div>
-</div>
