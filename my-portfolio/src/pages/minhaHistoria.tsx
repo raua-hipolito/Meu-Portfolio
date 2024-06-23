1df5934be5ddf522ee579c050d1684728fda8e73
Rauã <rauavasconcelos22@gmail.com>
2024-06-23 13:38:41 -0300

    🐛

--- my-portfolio/src/pages/minhaHistoria.tsx
+++ my-portfolio/src/pages/minhaHistoria.tsx
@@ -36,6 +36,7 @@ function minhaHistoria() {
             />
           </div>
           <p className="font-mono text-justify text-2xl tracking-tight md:tracking-normal md:text-3xl lg:text-4xl">
+            <br />
           Sou apaixonado por tecnologia, buscando constantemente aprender e crescer profissionalmente. Estou aberto a novas oportunidades e colaborações que me permitam contribuir com minha experiência e habilidades. Vamos conectar e explorar possíveis sinergias! 🚀
           </p>
       <Footer /> 
